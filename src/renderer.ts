dd82e014807da20d98b7ec61ebbfdc1ba8e65f56
Wesley Zloza <wesleyzloza@gmail.com>
2026-03-24 01:25:33 -0500

    Configured Vue per Electron Forge tutorial.

--- src/renderer.ts
+++ src/renderer.ts
@@ -27,7 +27,7 @@
  */
 
 import './index.css';
+import { createApp } from 'vue';
+import App from './App.vue';
 
-console.log(
-  '👋 This message is being logged by "renderer.ts", included via Vite',
-);
+createApp(App).mount('#app');
